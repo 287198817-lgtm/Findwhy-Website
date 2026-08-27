9b2795d7efa400fae1367c041cb2653f4da91c6c
287198817-lgtm <287198817@qq.com>
2026-08-27 12:55:45 +0800

    chore: remove legacy Keystatic admin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import keystatic from '@keystatic/astro';
-import markdoc from '@astrojs/markdoc';
-import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
 	adapter: vercel(),
-	integrations: [react(), markdoc(), keystatic()],
 });
